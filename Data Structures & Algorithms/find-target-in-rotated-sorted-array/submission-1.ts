5b0ace760493e4ea3aa2e9348ac571cc7d556b84
Faique Ahmad Khan <fkhan.bese20seecs@seecs.edu.pk>
2026-07-08 17:40:34 +0500

    Add: find-target-in-rotated-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-1.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums: number[], target: number): number {
+        let left = 0;
+        let right = nums.length - 1;
+
+        while (right >= left) {
+            const mid = Math.floor((left + right) / 2);
+            if (nums[mid] === target) return mid;
+
+            if (nums[mid] >= nums[left]) {
+                if (nums[mid] > target && nums[left] <= target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[right] >= target && nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
