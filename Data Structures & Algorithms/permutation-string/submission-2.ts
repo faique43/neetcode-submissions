3792a2dac55aa44c5c5e41254900bff9497d315e
Faique Ahmad Khan <fkhan.bese20seecs@seecs.edu.pk>
2026-07-08 16:59:57 +0500

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.ts
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+
+    checkInclusion(s1: string, s2: string): boolean {
+        let left = 0;
+        let right = s1.length;
+
+        function isAnagram(s: string, t: string): boolean {
+            if (s.length !== t.length) return false;
+
+            const count = new Array(26).fill(0);
+            const a = "a".charCodeAt(0);
+
+            for (let i = 0; i < s.length; i++) {
+                count[s.charCodeAt(i) - a]++; // add letters from s
+                count[t.charCodeAt(i) - a]--; // remove letters from t
+            }
+
+            return count.every((c) => c === 0);
+        }
+
+        while (right > left && right <= s2.length) {
+            if (isAnagram(s1, s2.slice(left, right))) {
+                return true;
+            } else {
+                left++;
+                right++;
+            }
+        }
+
+        return false;
+    }
+}
